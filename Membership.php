9649aaaf824c5728c994faca8731552c539292b9
itgsod-Axel-IngelssonFredler <axel.ingelssonfredler@elev.it-gymnasiet.se>
2016-04-12 19:48:36 +0200

    Worked on Database for the website's leaderboards function

---
+++ Membership.php
@@ -0,0 +1,8 @@
+<?php
+
+class Membership {
+	
+	function validate_user($user_name, $password) { 
+	
+	}
+}
---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'axelif');
+define('DB_PASSWORD', 'krunkola');
+define('DB_NAME', 'membership');
+
+?>
