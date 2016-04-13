1410a7e1f8ebff9b4b7591ef509b2123dc37171a
itgsod-Axel-IngelssonFredler <axel.ingelssonfredler@elev.it-gymnasiet.se>
2016-04-13 09:49:27 +0200

    Added new HUD

--- Membership.php
+++ Membership.php
@@ -1,8 +1,17 @@
 <?php
 
+require 'Mysql.php';
+
 class Membership {
 	
-	function validate_user($user_name, $password) { 
+	function validate_user($user_name, $password) {
+		$mysql = New Mysql();
+		$ensure_credentials = $mysql->verify_Username_and_Pass($user_name, $password);
+		
+		if($ensure_credentials) {
+			$SESSION['status'] = 'authorized';
+			header("location: index.erb");
+			} else return "Please enter a correct username or password";
 	
 	}
 }
--- Mysql.php
+++ Mysql.php
@@ -1,11 +1,34 @@
 <?php
 
-require_once(
+require_once 'constants.php';
 
 class MySQL {
 	
+	private $connection;
+	
+	function _construct() {
+		$this->connection = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or 
+							  die('Couldnt connect to the database');
+	
+	}
+	
 	function verify_Username_and_Pass($user_name, $password) {
-		$connection = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or 
-			die('Couldnt connect to the database')
+		
+			$query = "SELECT * 
+					 FROM users
+					 WHERE username = ? AND password = ?
+					 LIMIT 1";
+					 
+			if($stmt = $this->connection->prepare($query)) {
+				$stmt->bind_param('ss', $user_name, $password);
+				$stmt->execute();
+				
+				if($stmt->fetch()) {
+					$stmt->close();
+					return true;
+				}
+			
+			}
+			
 	}
 }
